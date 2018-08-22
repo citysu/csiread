04d6b12390bc06d0bd4325882c5f029d80bb911c
suhecheng <2215523266@qq.com>
2018-08-22 22:36:30 +0800

    ready for pypi
    use setuptools instead of distutils, 
    
    new directory structure, add sample data.

--- setup.py
+++ setup.py
@@ -2,29 +2,48 @@
 # -*- coding: utf-8 -*-
 
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
 from Cython.Build import cythonize
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+extensions = [
+    Extension(
+        "csiread", ["csiread/csiread.pyx"], 
+        extra_compile_args=['-Wno-cpp']
+    ),
+]
 setup(
     name = "csiread",
     version = "1.0.3",
-    description = "Parse binary file obtained by csi-tool",
-    long_description = "you can get csi data from Intel 5300 NIC, instead of Matlab, read csi data by python, and do processing.",
+
     author = "suhecheng",
     author_email = "2215523266@qq.com",
+
+    description = "Parse binary file obtained by csi-tool",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+
     maintainer = "suhecheng",
     maintainer_email = "2215523266@qq.com",
-    url = "nothing",
-    download_url = "nothing",
-    packages = [],
-    py_modules = [],
-    scripts = [],
-    ext_modules = cythonize([
-        Extension("csiread", ["csiread.pyx"], extra_compile_args=['-Wno-cpp'])
-    ]),
+
+    packages = find_packages(),
+    package_data = {
+        'csiread.sample': ['*']
+    },
+    install_requires = [
+        'numpy'
+    ],
+    python_requires = '>=3',
+
+    ext_modules = cythonize(extensions),
+
     classifiers = ["Topic :: Scientific/Engineering",
-                    "Programming Language :: Python :: Implementation :: CPython"],
-    data_files = [],
-    package_dir = []  
+                    "Programming Language :: Python :: 3"
+                    "Programming Language :: Python :: Implementation :: CPython", 
+                    "Operating System :: POSIX :: Linux",
+                    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
+
 )
