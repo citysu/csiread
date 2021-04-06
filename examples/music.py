7dde56dd74ab5a4597f657596171e43c61ea719d
suhecheng <2215523266@qq.com>
2021-04-06 18:34:11 +0800

    Enhance music.py: Add Spatial Smoothing of Correlated Sources

--- examples/music.py
+++ examples/music.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""MUSIC Algorithm
+"""MUltiple SIgnal Classification (MUSIC) Algorithm
 
 Usage:
-    python3 csimusic.py
+    python3 music.py
 
 Ref:
    1. [MUSIC Super-Resolution DOA Estimation](https://ww2.mathworks.cn/help/phased/ug/music-super-resolution-doa-estimation.html)
@@ -12,92 +12,102 @@ Ref:
    3. 王永良. 空间谱估计理论与算法[M]. 清华大学出版社, 2004.
 """
 
-import numpy as np
-from numpy import linalg as LA
-from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
+import numpy as np
+from numpy.linalg import eigh, norm, inv
+from numpy.random import randn, uniform
+from scipy.linalg import hankel
+from scipy.signal import find_peaks
+from scipy.stats import gmean
 
 
-def signalG(N, M, L, d, f, c=3e8):
-    """Signal Generation(Uncorrelated Sources)
+def signalG(T, M, D, d, f, c=3e8, correlated=False):
+    """Signal Generation
 
     Args:
-        N: number of snapshots
+        T: number of snapshots
         M: number of receivers
-        L: number of sources
+        D: number of sources
         d: distance between receivers
         f: frequency
         c: wave speed
+        correlated: correlated sources or not
     """
     ula = np.c_[:M] * d                                         # uniform linear array (ULA)
-    doa = np.sort(np.random.uniform(-np.pi/2, np.pi/2, L))      # directions of arrival (DOA)
+    doa = np.sort(uniform(-np.pi/2, np.pi/2, D))                # directions of arrival (DOA)
     a = np.exp(-2.j * np.pi * f * ula * np.sin(doa) / c)        # steering vector
-    s = np.random.randn(L, N) + np.random.randn(L, N) * 1.j     # source signal
-    n = np.random.randn(M, N) + np.random.randn(M, N) * 1.j     # noise
+    s = (randn(T) + randn(T) * 1.j) * np.exp(1.j * np.c_[:D]) \
+        if correlated else randn(D, T) + randn(D, T) * 1.j      # sources
+    n = randn(M, T) + randn(M, T) * 1.j                         # noise
     x = a @ s + n * 0.01                                        # signal model
     return x.T, doa
 
 
-def NoSS(X, method='AIC'):
-    """Number of Signal Source"""
-    N, M = X.shape
-    R = (X[..., np.newaxis] @ X[:, np.newaxis].conj()).mean(axis=0)
-    w, v = LA.eigh(R)
-
-    def Lambda(n):
-        return 1 / (M - n) * np.sum(w[:-n]) / np.prod(w[:-n]) ** (1 / (M - n))
-
-    def AIC(n):
-        return 2 * N * (M - n) * np.log(Lambda(n)) + 2 * n * (2 * M - n)
-
-    def MDL(n):
-        return N * (M - n) * np.log(Lambda(n)) + 1/2 * n * (2 * M - n) * np.log(N)
-
-    def HQ(n):
-        return N * (M - n) * np.log(Lambda(n)) + 1/2 * n * (2 * M - n) * np.log(np.log(N))
-
-    func = eval(method)
-    L = np.argmin([func(i) for i in range(1, M)]) + 1
-    return L
-
-
-def music(X, L, d, f, c=3e8):
-    """MUSIC Algorithm
+def smooth(X, L):
+    """Spatial Smoothing of Correlated Sources
 
     Args:
-        X: signal received [snapshots, receivers]
-        L: number of sources
-        d: distance between receivers
-        f: frequency
-        c: wave speed
+        L: The number of elements in each subarray
     """
-    N, M = X.shape
-    R = (X[..., np.newaxis] @ X[:, np.newaxis].conj()).mean(axis=0)
-    w, v = LA.eigh(R)
-    u = v[:, ::-1][:, L:]                                       # noise subspace
+    T, M = X.shape
+    J = M - L + 1
+    X = [hankel(X[i, :L], X[i, -J:]).T for i in range(T)]
+    X = np.asarray(X).reshape(-1, L)
+    return X
 
-    ula = np.c_[:M] * d                                         # ULA  
+
+def NoSS(X, method='MDL'):
+    """Number of Signal Source"""
+    T, M = X.shape
+    R = (X[..., np.newaxis] @ X[:, np.newaxis].conj()).mean(axis=0)
+    w, v = eigh(R)
+
+    def Lambda(n):
+        return np.mean(w[:-n]) / gmean(w[:-n])
+
+    def AIC(n):
+        return 2 * T * (M - n) * np.log(Lambda(n)) + 2 * n * (2 * M - n)
+
+    def MDL(n):
+        return T * (M - n) * np.log(Lambda(n)) + 1/2 * n * (2 * M - n) * np.log(T)
+
+    def HQ(n):
+        return T * (M - n) * np.log(Lambda(n)) + 1/2 * n * (2 * M - n) * np.log(np.log(T))
+
+    func = eval(method)
+    D = np.argmin([func(i) for i in range(1, M)]) + 1
+    return D
+
+
+def music(X, D, d, f, c=3e8):
+    """Classic MUSIC Algorithm"""
+    T, M = X.shape
+    R = (X[..., np.newaxis] @ X[:, np.newaxis].conj()).mean(axis=0)
+    w, v = eigh(R)
+    u = v[:, ::-1][:, D:]                                       # noise subspace
+
+    ula = np.c_[:M] * d                                         # ULA
     doa_space = np.linspace([[-np.pi/2]], [[np.pi/2]], 180)     # search space
     a = np.exp(-2.j * np.pi * f * ula * np.sin(doa_space) / c)  # steering vector
-    p = 1 / LA.norm(u.T.conj() @ a, axis=(1, 2))                # MUSIC pseudospectrum
-    # p *= LA.norm(a, axis=(1, 2))                                # optional
+    p = 1 / norm(u.T.conj() @ a, axis=(1, 2))                   # MUSIC pseudospectrum
+    p *= norm(a, axis=(1, 2))                                   # optional
     p = 10 * np.log10(p/p.max())
 
     # DOA
     peak, _ = find_peaks(p)
-    index = sorted(peak[p[peak].argsort()][-L:])
+    index = sorted(peak[p[peak].argsort()][-D:])
     doa = doa_space[index, 0, 0]
     return p, doa
 
 
-def music_root(X, L, d, f, c=3e8):
+def music_root(X, D, d, f, c=3e8):
     """Root-MUSIC(RMU) Algorithm"""
-    N, M = X.shape
+    T, M = X.shape
     R = (X[..., np.newaxis] @ X[:, np.newaxis].conj()).mean(axis=0)
-    w, v = LA.eigh(R)
-    u = v[:, ::-1][:, L:]
+    w, v = eigh(R)
+    u = v[:, ::-1][:, D:]
 
-    coff = np.r_[1, (u[:L] @ LA.inv(u[L:]))[::-1, 0]]
+    coff = np.r_[1, (u[:D] @ inv(u[D:]))[::-1, 0]]
     r = np.roots(coff)
     doa = np.sort(np.arcsin(np.angle(r) * c / f / (2 * np.pi * d)))
     return None, doa
@@ -108,9 +118,10 @@ def plotspectrum(p, doa_fake, doa_real):
     doa_fake *= (180 / np.pi)
     print("signalG - doa: ", doa_real)
     print("MUSIC   - doa: ", doa_fake)
-    
+
     if p is None:
         p = np.sin(np.deg2rad(np.arange(180)))
+
     x = np.linspace(-90, 90, p.size)
     index = [np.abs(x - a).argmin() for a in doa_fake]
 
@@ -127,7 +138,7 @@ def plotspectrum(p, doa_fake, doa_real):
 
 
 if __name__ == "__main__":
-    x, doa_real = signalG(N=100, M=4, L=3, d=6.25e-2, f=2.4e9)
-    p, doa_fake = music(X=x, L=NoSS(x), d=6.25e-2, f=2.4e9)
-
+    x, doa_real = signalG(T=100, M=9, D=3, d=6.25e-2, f=2.4e9, correlated=True)
+    x = smooth(x, L=5)
+    p, doa_fake = music(X=x, D=NoSS(x), d=6.25e-2, f=2.4e9)
     plotspectrum(p, doa_fake, doa_real)
