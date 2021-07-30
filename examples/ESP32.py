4431aef277cfc6efa4f53d2c53401c3447d2ce2d
suhecheng <2215523266@qq.com>
2021-07-30 12:26:31 +0800

    New example: read esp32 csi files with pandas(#10)

---
+++ examples/ESP32.py
@@ -0,0 +1,35 @@
+"""ESP32-CSI-Tool
+
+Read esp32 csi files flexibly.
+"""
+
+import numpy as np
+import pandas as pd
+from timeit import default_timer
+
+
+def read_esp32(file):
+    dtype = {'type': str, 'role': str, 'mac': str, 'rssi': int, 'rate': int,
+             'sig_mode': int, 'mcs': int, 'bandwidth': int, 'smoothing': int,
+             'not_sounding': int, 'aggregation': int,'stbc': int,
+             'fec_coding': int, 'sgi': int, 'noise_floor': int,
+             'ampdu_cnt': int, 'channel': int, 'secondary_channel': int,
+             'local_timestamp': int, 'ant': int, 'sig_len': int,
+             'rx_state': int, 'real_time_set': int, 'real_timestamp': float,
+             'len': int, 'csi_data': object}
+    names = list(dtype.keys())
+    data = pd.read_csv(file, header=None, names=names, dtype=dtype,
+                       usecols=['csi_data'], engine='c')
+
+    # parse csi
+    csi_string = ''.join(data['csi_data'].apply(lambda x: x.strip('[]')))
+    csi = np.fromstring(csi_string, dtype=int, sep=' ').reshape(-1, 64)
+    csi = csi[1::2] + csi[::2] * 1.j
+
+    return csi
+
+if __name__ == '__main__':
+    last = default_timer()
+    csifile = '../material/esp32/dataset/example_csi.csv'
+    read_esp(csifile)
+    print(default_timer() - last, 's')
