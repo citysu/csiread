e9c8fc293cae2a8d34d5c3ff81776f72bca66043
suhecheng <2215523266@qq.com>
2021-07-31 15:30:45 +0800

    Enhance csiNexSpatial.py

--- examples/csiNexSpatial.py
+++ examples/csiNexSpatial.py
@@ -28,55 +28,73 @@ def loadcsi(csifile):
     return csidata
 
 
-def group(csidata):
-    seq_diff = np.diff(csidata.seq)
-    offset = np.where(seq_diff != 0)[0]
-    offset = np.r_[0, offset + 1]
-    count = np.diff(np.r_[offset, csidata.count])
-    return offset, count
+def get_frames_index(seq, ant_num=16):
+    """Get the index of the frames
 
-
-def rearrange(amp, ant_num=16):
-    """Rearrange antennas by shortest distance of amplitude
+    Adjacent packets with the same sequence number are from the same frame
 
     Args:
-        amp: amplitude, [antennas, packets_num, subcarriers]
-        ant_num: antennas number
+        seq: sequence number
+        ant_num: antennas count
 
-    Return:
-        amp: resorted amplitude.
-        idx: new antenna index
+    Attributes:
+        count: the count of packets corresponding to one frame. we only keep
+            frames where `count == ant_num`
+
+    Returns:
+        ndarray: offset, the position of frames. shape=[frame_count, ant_num]
     """
-    idx = list()
-    for i in range(1, amp.shape[1]):
-        G, J = list(), list(range(ant_num))
-        for k in range(ant_num):
-            d = [np.linalg.norm(amp[j, i] - amp[k, 0], axis=-1) for j in J]
+    seq_diff = np.diff(seq)
+    offset = np.where(seq_diff != 0)[0]
+    offset = np.r_[0, offset + 1]
+    count = np.diff(np.r_[offset, len(seq)])
+    offset = offset[count == ant_num]
+    offset = [np.r_[o:o+ant_num] for o in offset]
+    offset = np.asarray(offset)
+    return offset
+
+
+def rearrange(offset, csi):
+    """Rearrange antennas by shortest distance of amplitudes
+
+    Args:
+        offset: returned by `get_frames_index`, shape=[frame_count, ant_num]
+        csi: csidata.csi, shape=[packets_num, subcarriers]
+
+    Returns:
+        ndarray: offset_new, the position of frames after rearranging antennas.
+            shape=[frame_count, ant_num]
+    """
+    offset_new = np.empty_like(offset)
+    packet_num, ant_num = offset.shape
+    amp = np.abs(csi[offset])
+    index_ant = np.r_[:ant_num]
+    for i in range(packet_num):
+        d = np.linalg.norm(amp[i, np.newaxis] - amp[0, :, np.newaxis], axis=-1)
+        d_max = d.max()
+        for j in range(ant_num):
             p = np.argmin(d)
-            G.append(J.pop(p))
-        # Rearrange
-        amp[:, i] = amp[G, i]
-        idx.append(G)
-    idx = np.asarray(idx).T
-    return amp, idx
+            px, py = p // ant_num, p % ant_num
+            d[px, :], d[:, py] = d_max, d_max
+            index_ant[px] = py
+        offset_new[i, :] = offset[i, index_ant]
+    return offset_new
 
 
 def plotting(csidata):
     s_index = scidx(80, 1, 'ac')
-    a_index = s_index + 80 // 20 * 32
-    offset, count = group(csidata)
-    offset, count = offset[count == 16], count[count == 16]
 
-    amp = [np.abs(csidata.csi[offset + i][:, a_index]) for i in range(16)]
-    amp = np.asarray(amp)   # [16, packets_num, subcarriers]
-    amp, idx = rearrange(amp, 16)
+    offset_0 = get_frames_index(csidata.seq, 16)
+    offset = rearrange(offset_0, csidata.csi)
+
     t = csidata.sec + csidata.usec * 1e-6
-    t = t[offset]
+    t = t[offset[:, 0]]
     t = t - t[0]
+    amp = np.abs(csidata.csi[offset])
 
     plt.figure(figsize=(8, 4))
-    plt.imshow(idx, cmap='jet', aspect='auto', interpolation='antialiased',
-               origin='lower')
+    plt.imshow(offset.T - offset_0[:, :1].T, cmap='jet', aspect='auto',
+               interpolation='antialiased', origin='lower')
     plt.xlabel('packets')
     plt.ylabel('antennas')
     plt.tight_layout()
@@ -84,7 +102,7 @@ def plotting(csidata):
     fig, axes = plt.subplots(4, 4, sharex=True, sharey=True, figsize=(8, 4))
     for i in range(4):
         for j in range(4):
-            axes[i, j].imshow(amp[4 * i + j].T, cmap='jet', aspect='auto',
+            axes[i, j].imshow(amp[:, 4 * i + j].T, cmap='jet', aspect='auto',
                               origin='lower',
                               extent=(t[0], t[-1], s_index[0], s_index[-1]))
     fig.text(0.5, 0.005, 'time [s]', ha='center')
